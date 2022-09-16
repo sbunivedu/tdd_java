37a03b40945e9e70e65bec7da27847ec9ce20121
Baochuan Lu <lubaochuan@gmail.com>
2022-09-16 14:13:09 -0500

    make test 4 pass

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -2,6 +2,8 @@ public class Fizzbuzz{
   public String getResult(int number) {
     if(number%3 == 0){
       return "Fizz";
+    }else if(number%5 == 0){
+      return "Buzz";
     }
     return String.valueOf(number);
   }
