a18594996c20450718b4662c3d48fa3c7694022c
Baochuan Lu <lubaochuan@gmail.com>
2022-09-16 13:58:16 -0500

    add test 3 and watch it fail

--- FizzbuzzTest.java
+++ FizzbuzzTest.java
@@ -8,11 +8,18 @@ public class FizzbuzzTest{
     String result = fizzbuzz.getResult(1);
     assertEquals("1", result);
   }
-  
+
   @Test
   public void returnsTwoIfGivenTwo() {
     Fizzbuzz fizzbuzz = new Fizzbuzz();
     String result = fizzbuzz.getResult(2);
     assertEquals("2", result);
+  }
+
+  @Test
+  public void returnsFizzIfGivenThree() {
+    Fizzbuzz fizzbuzz = new Fizzbuzz();
+    String result = fizzbuzz.getResult(3);
+    assertEquals("Fizz", result);
   }
 }
